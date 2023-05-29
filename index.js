0c38ed71d58e48c59cf4ec9d491d22f48ecb736e
shawnbradyrobinson <shawnbradyrobinson1@gmail.com>
2023-05-29 13:53:40 -0500

    developed a strategy for how to tackle the challenge

---
+++ index.js
@@ -0,0 +1,60 @@
+/**
+ * Inquirer needs to ask the user a series of questions, and then the answers need to call the separate operations
+ * 
+ * 
+ * 
+ * 1) Ask for three characters (else too many characters)
+ * 
+ * 2) Accepts text color keywords, or hexadecimal color coding..."Do you want to enter color as keyword or hexadecimal?"
+ * 
+ * 3) Choose a shape 
+ * 
+ * 4) Accepts shape color keywords...
+ * 
+ * 5) Writes the file to logo.svg and outputs the text "Generated logo.svg"
+ * 
+ * Probably a good idea to have a CLI Class like the mini project has 
+ * 
+ * SHAPE CLASS 
+ * 
+ * class Shape {
+ * 
+ * PROPERTIES 
+ * whatever dimensions svg needs to make these shapes...the general shape class could start with zeroes for all...
+ * METHODS
+ * 
+ * const textSize = ??? <-- maybe this needs to change depending on the different shapes, or maybe it can be uniform, either way, making it a property and then referring to it later will help out, I believe. 
+ * 
+ * renderShape(){
+ * //polymorphism changes this one depending on the shape 
+ * 
+ * }
+ * 
+ * renderTextColor(){
+ * //this could stay the same for all shapes
+ * 
+ * }
+ * 
+ * renderShapeColor(){
+ * //this could stay the same for all shapes 
+ * 
+ *}
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * }
+ * Alright, so there's some basic shape-ness, and then, like with the mini-project, the details are filled in by its children...
+ * 
+ * 
+ * 
+ * 
+ 
+ */
+
+
+
